b97a636ef2dd1397cd5050e4c25a061ec4d6895d
Kyle Daruwalla <daruwalla@wisc.edu>
2021-06-15 21:34:49 -0500

    Simplifying rrules

--- src/fastpaths.jl
+++ src/fastpaths.jl
@@ -8,30 +8,22 @@ for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
         NNlib.$fconv(x::AbstractArray{xT, N}, w::$WT{wT, N}, cdims::ConvDims;
                      kwargs...) where {xT, wT, N} = NNlib.$fconv(x, freeze(w), cdims; kwargs...)
 
-        function ChainRulesCore.rrule(::typeof($fconv), x, w::$WT, cdims; kw...)
+        function ChainRulesCore.rrule(::typeof($fconv), x, w::$WT, cdims; kwargs...)
+            y, pb = ChainRulesCore.rrule($fconv, x, freeze(w), cdims; kwargs...)
             function $conv_pullback(Δ)
-                Δ = colmajor(Δ)
-                return (
-                    NoTangent(),
-                    @thunk($∇conv_data(Δ, freeze(w), cdims, kw...)),
-                    @thunk(w.mask .* $∇conv_filter(x, Δ, cdims, kw...)),
-                    NoTangent(),
-                )
+                Δs = pb(Δ)
+                return (Δs[1], Δs[2], w.mask .* Δs[3], Δs[4])
             end
-            return $conv(x, w, cdims; kw...), $conv_pullback
+            return $fconv(x, w, cdims; kw...), $conv_pullback
         end
 
-        function ChainRulesCore.rrule(::typeof($∇conv_data), x, w::$WT, cdims; kw...)
+        function ChainRulesCore.rrule(::typeof($∇conv_data), x, w::$WT, cdims; kwargs...)
+            y, pb = ChainRulesCore.rrule($∇conv_data, x, freeze(w), cdims; kwargs...)
             function $∇conv_data_pullback(Δ)
-                Δ = colmajor(Δ)
-                return (
-                    NoTangent(),
-                    @thunk($conv(Δ, freeze(w), cdims, kw...)),
-                    @thunk(w.mask .* $∇conv_filter(Δ, x, cdims, kw...)),
-                    NoTangent(),
-                )
+                Δs = pb(Δ)
+                return (Δs[1], Δs[2], w.mask .* Δs[3], Δs[4])
             end
-            return $∇conv_data(x, w, cdims; kw...), $∇conv_data_pullback
+            return y, $∇conv_data_pullback
         end
     end
 end
