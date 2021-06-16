8ecc2fe578eeb1bc21d7e86e05c297ac08477470
Kyle Daruwalla <daruwalla@wisc.edu>
2021-06-15 19:59:56 -0500

    Add conv defs with cdims included

--- src/fastpaths.jl
+++ src/fastpaths.jl
@@ -1,3 +1,6 @@
-for fconv in [:conv, :depthwiseconv], WT in [MaskedArray, MaskedSliceArray]
-    @eval NNlib.$fconv(x, w::$WT; kwargs...) = NNlib.$fconv(x, freeze(w); kwargs...)
+for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
+    @eval begin
+        NNlib.$fconv(x, w::$WT; kwargs...) = NNlib.$fconv(x, freeze(w); kwargs...)
+        NNlib.$fconv(x, w::$WT, cdims::ConvDims; kwargs...) = NNlib.$fconv(x, freeze(w), cdims; kwargs...)
+    end
 end
