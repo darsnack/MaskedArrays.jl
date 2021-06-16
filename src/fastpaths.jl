2f8de4f37a81080b17d44499c5e3ba2790a20638
Kyle Daruwalla <daruwalla@wisc.edu>
2021-06-15 21:21:48 -0500

    Fix precompile errors

--- src/fastpaths.jl
+++ src/fastpaths.jl
@@ -1,14 +1,14 @@
 for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
+    local ∇conv_data, ∇conv_filter = Symbol.(:∇, fconv, [:_data, :_filter])
+    conv_pullback, ∇conv_data_pullback = Symbol.([fconv, ∇conv_data], :_pullback)
+
     @eval begin
         NNlib.$fconv(x::AbstractArray{xT, N}, w::$WT{wT, N}; kwargs...) where {xT, wT, N} =
             NNlib.$fconv(x, freeze(w); kwargs...)
         NNlib.$fconv(x::AbstractArray{xT, N}, w::$WT{wT, N}, cdims::ConvDims;
                      kwargs...) where {xT, wT, N} = NNlib.$fconv(x, freeze(w), cdims; kwargs...)
 
-        local ∇conv_data, ∇conv_filter = Symbol.(:∇, conv, [:_data, :_filter])
-        conv_pullback, ∇conv_data_pullback = Symbol.([conv, ∇conv_data], :_pullback)
-
-        @eval function ChainRulesCore.rrule(::typeof($conv), x, w::$WT, cdims; kw...)
+        function ChainRulesCore.rrule(::typeof($fconv), x, w::$WT, cdims; kw...)
             function $conv_pullback(Δ)
                 Δ = colmajor(Δ)
                 return (
@@ -21,7 +21,7 @@ for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
             return $conv(x, w, cdims; kw...), $conv_pullback
         end
 
-        @eval function ChainRulesCore.rrule(::typeof($∇conv_data), x, w::$WT, cdims; kw...)
+        function ChainRulesCore.rrule(::typeof($∇conv_data), x, w::$WT, cdims; kw...)
             function $∇conv_data_pullback(Δ)
                 Δ = colmajor(Δ)
                 return (
