08dc87e8ced2accd10c4a6d64da8776ff1015b74
Kyle Daruwalla <daruwalla@wisc.edu>
2021-06-15 20:09:27 -0500

    Address method ambiguities

--- src/fastpaths.jl
+++ src/fastpaths.jl
@@ -1,6 +1,8 @@
 for fconv in (:conv, :depthwiseconv), WT in (MaskedArray, MaskedSliceArray)
     @eval begin
-        NNlib.$fconv(x, w::$WT; kwargs...) = NNlib.$fconv(x, freeze(w); kwargs...)
-        NNlib.$fconv(x, w::$WT, cdims::ConvDims; kwargs...) = NNlib.$fconv(x, freeze(w), cdims; kwargs...)
+        NNlib.$fconv(x::AbstractArray{xT, N}, w::$WT{wT, N}; kwargs...) where {xT, wT, N} =
+            NNlib.$fconv(x, freeze(w); kwargs...)
+        NNlib.$fconv(x::AbstractArray{xT, N}, w::$WT{wT, N}, cdims::ConvDims;
+                     kwargs...) where {xT, wT, N} = NNlib.$fconv(x, freeze(w), cdims; kwargs...)
     end
 end
