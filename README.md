4d245fe2a16013f1c17d4269923872887a777241
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-07 21:54:38 -0500

    Add documentation

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
 # MaskedArrays
 
 [![Build Status](https://github.com/darsnack/MaskedArrays.jl/workflows/CI/badge.svg)](https://github.com/darsnack/MaskedArrays.jl/actions)
+
+MaskedArrays.jl allows you to associated a bitmask with an array non-destructively. Any elements with a masked value of `false` are zero, while other elements are passed through. The original array is not modified. It provides two types: `MaskedArray` and `MaskedSliceArray`. Nothing is exported.
+
+```julia
+julia> x = rand(4, 5)
+4×5 Matrix{Float64}:
+ 0.45442   0.323381  0.245189   0.9963    0.901239
+ 0.867445  0.358103  0.428182   0.888031  0.800996
+ 0.116383  0.499893  0.0768902  0.614683  0.23086
+ 0.840305  0.564109  0.477627   0.104402  0.0891596
+
+julia> m = x .< 0.5
+4×5 BitMatrix:
+ 1  1  1  0  0
+ 0  1  1  0  0
+ 1  1  1  0  1
+ 0  0  1  1  1
+
+julia> mask(x, m)
+4×5 MaskedArray{Float64, 2, Matrix{Float64}}:
+ 0.45442   0.323381  0.245189   0.0       0.0
+ 0.0       0.358103  0.428182   0.0       0.0
+ 0.116383  0.499893  0.0768902  0.0       0.23086
+ 0.0       0.0       0.477627   0.104402  0.0891596
+```
+
+You can also pass a `Vector` of indices to mask elements.
+
+```julia
+julia> x = ones(3, 2)
+3×2 Matrix{Float64}:
+ 1.0  1.0
+ 1.0  1.0
+ 1.0  1.0
+
+julia> mask(x, [1, 5])
+3×2 MaskedArray{Float64, 2, Matrix{Float64}}:
+ 1.0  0.0
+ 0.0  1.0
+ 0.0  0.0
+```
+
+Even complete slices of dimensions can be masked.
+
+```julia
+julia> mask(x, 1:2, :, :)
+3×2×2 MaskedSliceArray{Float64, 3, Array{Float64, 3}, Tuple{UnitRange{Int64}, Base.Slice{Base.OneTo{Int64}}, Base.Slice{Base.OneTo{Int64}}}}:
+[:, :, 1] =
+ 0.69798   0.353225
+ 0.646014  0.20619
+ 0.0       0.0
+
+[:, :, 2] =
+ 0.318028  0.276218
+ 0.508333  0.274718
+ 0.0       0.0
+```
+
+The original array can be obtained with `unmask`, and the masking can be made permanent with `freeze`.
--- src/api.jl
+++ src/api.jl
@@ -13,10 +13,86 @@ function _slicetomask(A, slices::Tuple)
     return slicemask
 end
 
+"""
+    mask(A::AbstractArray, bitmask::AbstractArray{Bool})
+
+Mask `A` by zeroing out the elements corresponding to false in `bitmask`.
+
+# Examples
+
+```julia
+julia> x = rand(4, 5)
+4×5 Matrix{Float64}:
+ 0.45442   0.323381  0.245189   0.9963    0.901239
+ 0.867445  0.358103  0.428182   0.888031  0.800996
+ 0.116383  0.499893  0.0768902  0.614683  0.23086
+ 0.840305  0.564109  0.477627   0.104402  0.0891596
+
+julia> m = x .< 0.5
+4×5 BitMatrix:
+ 1  1  1  0  0
+ 0  1  1  0  0
+ 1  1  1  0  1
+ 0  0  1  1  1
+
+julia> mask(x, m)
+4×5 MaskedArray{Float64, 2, Matrix{Float64}}:
+ 0.45442   0.323381  0.245189   0.0       0.0
+ 0.0       0.358103  0.428182   0.0       0.0
+ 0.116383  0.499893  0.0768902  0.0       0.23086
+ 0.0       0.0       0.477627   0.104402  0.0891596
+```
+"""
 mask(A::AbstractArray, bitmask::AbstractArray{Bool}) = MaskedArray(A, bitmask)
+
+"""
+    mask(A::AbstractArray, bitmask::Vector{<:CartesianIndex})
+    mask(A::AbstractArray, bitmask::Vector{<:Integer})
+
+Mask `A` by zeroing out any elements at indices not in `bitmask`.
+
+# Examples
+
+```julia
+julia> x = ones(3, 2)
+3×2 Matrix{Float64}:
+ 1.0  1.0
+ 1.0  1.0
+ 1.0  1.0
+
+julia> mask(x, [1, 5])
+3×2 MaskedArray{Float64, 2, Matrix{Float64}}:
+ 1.0  0.0
+ 0.0  1.0
+ 0.0  0.0
+```
+"""
+mask(A::AbstractArray, bitmask::Vector{<:CartesianIndex}) = mask(A, _indextomask(A, bitmask))
 # must be Vector{<:Integer} not AbstractVector{<:Integer} to avoid method ambiguity
 mask(A::AbstractArray, bitmask::Vector{<:Integer}) = mask(A, _indextomask(A, bitmask))
-mask(A::AbstractArray, bitmask::Vector{<:CartesianIndex}) = mask(A, _indextomask(A, bitmask))
+
+"""
+    mask(A::AbstractArray, slices::Tuple)
+    mask(A::AbstractArray, slice, slices...)
+
+Mask a slice of `A` specified by the slices missing from `slices`.
+
+# Examples
+
+```julia
+julia> mask(x, 1:2, :, :)
+3×2×2 MaskedSliceArray{Float64, 3, Array{Float64, 3}, Tuple{UnitRange{Int64}, Base.Slice{Base.OneTo{Int64}}, Base.Slice{Base.OneTo{Int64}}}}:
+[:, :, 1] =
+ 0.69798   0.353225
+ 0.646014  0.20619
+ 0.0       0.0
+
+[:, :, 2] =
+ 0.318028  0.276218
+ 0.508333  0.274718
+ 0.0       0.0
+```
+"""
 mask(A::AbstractArray, slices::Tuple) = MaskedSliceArray(A, slices)
 mask(A::AbstractArray, slice, slices...) = mask(A, (slice, slices...))
 mask(A::MaskedArray, bitmask::AbstractArray{Bool}) = mask(A.data, A.mask .* bitmask)
@@ -26,8 +102,20 @@ mask(A::MaskedArray, slices::Tuple) = mask(A.data, A.mask .* _slicetomask(A, sli
 mask(A::MaskedSliceArray, bitmask::AbstractArray{Bool}) =
     mask(A.data, bitmask .* _slicetomask(A, A.slices))
 
+"""
+    unmask(A::MaskedArray)
+    unmask(A::MaskedSliceArray)
+
+Unmask `A` by returning the original data without a mask applied.
+"""
 unmask(A::MaskedArray) = A.data
 unmask(A::MaskedSliceArray) = A.data
 
+"""
+    freeze(A::MaskedArray)
+    freeze(A::MaskedSliceArray)
+
+Make a masked array permanent by returning the original data with the mask applied.
+"""
 freeze(A::MaskedArray) = A.data .* A.mask
 freeze(A::MaskedSliceArray) = A.data .* _slicetomask(A, A.slices)
